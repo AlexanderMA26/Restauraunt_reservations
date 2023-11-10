b19b8970d823638ae5b88ba9c45b86a02240fd49
dahtrgxnl <thelandisarnolds@gmail.com>
2023-11-10 14:44:48 -0500

    Day 3

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,14 @@ import java.util.Collections;
 import java.util.Random;
 
 
+
 public class Main {
     static ArrayList<Reservation> Resarray = new ArrayList<>();
+    static ArrayList<Reservation> resWaitList = new ArrayList<>();
 
     public static void main(String[] args) {
         Initialization();
         Input();
-
     }
 
     static void Initialization() {
@@ -23,25 +24,51 @@ public class Main {
     }
 
     static void newReservation() {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Input your party size");
-        int resSize = Integer.parseInt(scanner.nextLine());
+        if (Resarray.size() < 10){
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Input the time of your reservation (24-hour time)");
-        int parTime = Integer.parseInt(scanner.nextLine());
+            System.out.println("Input your party size");
+            int resSize = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("Give a name for your reservation");
-        String parName = scanner.nextLine();
+            System.out.println("Input the time of your reservation (24-hour time)");
+            int parTime = Integer.parseInt(scanner.nextLine());
 
-        int ResKey = Randint();
+            System.out.println("Give a name for your reservation");
+            String parName = scanner.nextLine();
+
+            int ResKey = Randint();
 
 
-        Resarray.add(new Reservation(resSize, parTime, parName, ResKey));
+            Resarray.add(new Reservation(resSize, parTime, parName, ResKey));
 
-        System.out.println("We have recieved your reservation!");
+            System.out.println("We have recieved your reservation!");
 
-        System.out.println("Your reservation key is " + ResKey + ". Don't forget your key!");
+            System.out.println("Your reservation key is " + ResKey + ". Don't forget your key!");
+        } else{
+
+            System.out.println("Because of the amount of current reservations, you will be put on the wait list. You will be number " + resWaitList.size() + "on the wait list.");
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Input your party size");
+            int resSize = Integer.parseInt(scanner.nextLine());
+
+            System.out.println("Input the time of your reservation (24-hour time)");
+            int parTime = Integer.parseInt(scanner.nextLine());
+
+            System.out.println("Give a name for your reservation");
+            String parName = scanner.nextLine();
+
+            int ResKey = Randint();
+
+
+            resWaitList.add(new Reservation(resSize, parTime, parName, ResKey));
+
+            System.out.println("We have recieved your reservation!");
+
+            System.out.println("Your reservation key is " + ResKey + ". Don't forget your key!");
+        }
+
 
         sortList();
 
@@ -50,30 +77,50 @@ public class Main {
 
     static void viewReservation() {
         sortList();
+        System.out.println("Current Reservations");
         for (Reservation obj : Resarray) {
             System.out.println();
             System.out.println("Reservation Name: " + obj.partyName);
-            System.out.println("Reservation Time: " + obj.resTime);
+            System.out.println("Reservation Time: " + obj.partyTime);
             System.out.println("Reservation Size: " + obj.partySize);
+        }
+        if (!resWaitList.isEmpty()){
+            System.out.println("\nCurrent Wait List");
+            for (Reservation obj : resWaitList){
+                System.out.println();
+                System.out.println("Reservation Name: " + obj.partyName);
+                System.out.println("Reservation Time: " + obj.partyTime);
+                System.out.println("Reservation Size: " + obj.partySize);
+        }
+
         }
         Input();
     }
 
     static void Input() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("\nEnter 'R' for existing reservations\nEnter 'N' to create a new reservation\nEnter 'C' to cancel a reservation");
+        System.out.println("\nEnter 'R' for existing reservations" +
+                "\nEnter 'N' to create a new reservation" +
+                "\nEnter 'C' to cancel a reservation" +
+                "\nEnter 'E' to change an existing reservation" +
+                "\nEnter 'A' to escape the screen");
 
         String beginningQuestions = scanner.nextLine();
 
-        if (beginningQuestions.equalsIgnoreCase("N")) {
-            newReservation();
-        } else if (beginningQuestions.equalsIgnoreCase("R")) {
-            viewReservation();
-        } else {
-            cancel();
+        switch (beginningQuestions.toLowerCase()){
+            case "n":
+                newReservation();
+            case "r":
+                viewReservation();
+            case "c":
+                cancel();
+            case "e":
+                changeRes();
+            case "a":
+                System.exit(0);
         }
 
-        scanner.close();
+
 
 
     }
@@ -87,11 +134,13 @@ public class Main {
 
         Resarray.removeIf(obj -> Objects.equals(obj.ResKey, groupToCancel));
 
+        System.gc();
+
 
         viewReservation();
 
 
-        scanner.close();
+
     }
 
 
@@ -105,7 +154,7 @@ public class Main {
         int n = Resarray.size();
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                if (Resarray.get(j) != null && Resarray.get(j + 1) != null && Resarray.get(j).resTime > Resarray.get(j + 1).resTime) {
+                if (Resarray.get(j) != null && Resarray.get(j + 1) != null && Resarray.get(j).partyTime > Resarray.get(j + 1).partyTime) {
                     Collections.swap(Resarray, j, j + 1);
                 }
             }
@@ -115,15 +164,50 @@ public class Main {
     static void changeRes(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the key of the reservation you want to change");
-
         String ResChange = scanner.nextLine();
 
+
+        int changing = Integer.parseInt(ResChange);
+
+        for (int i = 0; i <= Resarray.size(); i++){
+            if (changing == Resarray.get(i).ResKey){
+                Scanner scanner1 = new Scanner(System.in);
+                System.out.println("Do you want to change: The [T]ime, the [N]ame, or the [S]ize of the party?");
+                String change = scanner1.nextLine();
+
+
+
+                if (change.equalsIgnoreCase("t") || change.equalsIgnoreCase("time")){
+                    Scanner time = new Scanner(System.in);
+                    System.out.println("When would you like to change your reservation to?");
+                    String timeChange = time.nextLine();
+                    Resarray.get(i).partyTime = Integer.parseInt(timeChange);
+                    System.out.println("Your reservation time has been changed to " + Resarray.get(i).partyTime);
+
+                } else if (change.equalsIgnoreCase("n") || change.equalsIgnoreCase("name")){
+                    Scanner name = new Scanner(System.in);
+                    System.out.println("What is the new name of your party?");
+                    String nameChange = name.nextLine();
+                    Resarray.get(i).partyName = nameChange;
+                    System.out.println("The name of your party has been changed to " + Resarray.get(i).partyName);
+                    
+                } else if ((change.equalsIgnoreCase("s") || change.equalsIgnoreCase("size"))) {
+                    Scanner size = new Scanner(System.in);
+                    System.out.println("How many people are now coming?");
+                    String sizeChange = size.nextLine();
+                    Resarray.get(i).partySize = Integer.parseInt(sizeChange);
+                    System.out.println("Your party size is now" + Resarray.get(i).partySize);
+                }else{
+                    System.out.println("Invalid Input");
+                    changeRes();
+                }
+
+                break;
+            }
+        }
+
+
     }
-
-
-
-
-
 }
 
 
--- src/Reservation.java
+++ src/Reservation.java
@@ -2,16 +2,20 @@ import java.awt.*;
 
 public class Reservation {
 
-        public int resTime;
+        public int partyTime;
 
         public int partySize;
         public String partyName;
 
         public int ResKey;
 
+
+
+
+
     public Reservation(int pResTime, int pPartySize, String pPartyName, int pResKey) {
 
-        resTime = pResTime;
+        partyTime = pResTime;
         partySize = pPartySize;
         partyName = pPartyName;
         ResKey = pResKey;
